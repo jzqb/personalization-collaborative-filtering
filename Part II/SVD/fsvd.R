e3d20d07f93a2c05ed90bc8ea26b801a311bf137
ccw2145 <31906143+ccw2145@users.noreply.github.com>
2017-12-15 20:33:30 -0500

    Update fsvd.R

--- Part II/SVD/fsvd.R
+++ Part II/SVD/fsvd.R
@@ -42,68 +42,70 @@ train_prop = 0.8
 data <- train_test_matrix(training_data, testing_data, 0.8)
 train_matrix <- data$train_matrix
 test_matrix <- data$test_matrix
-
+## @knitr ignore-1
 #saveRDS(data,"SVD/data/data")
 
 ## @knitr fsvd_models
 #fsvd
-k= c(5,8,10,20,30)
+k= c(5,10,20,30)
 gamma = 0.015 #learning rates
 lambda = 0.001 #regularization
 fsvd_model_5 <- funkSVD(train_matrix, k = 5, gamma = 0.015, lambda = 0.001,min_improvement = 1e-06, min_epochs = 50, max_epochs = 200, verbose = FALSE)
-fsvd_model_8 <- funkSVD(train_matrix, k = 8, gamma = 0.015, lambda = 0.001,min_improvement = 1e-06, min_epochs = 50, max_epochs = 200, verbose = FALSE)
 fsvd_model_10 <- funkSVD(train_matrix, k = 10, gamma = 0.015, lambda = 0.001,min_improvement = 1e-06, min_epochs = 50, max_epochs = 200, verbose = FALSE)
 fsvd_model_20 <- funkSVD(train_matrix, k = 20, gamma = 0.015, lambda = 0.001,min_improvement = 1e-06, min_epochs = 50, max_epochs = 200, verbose = FALSE)
 fsvd_model_30 <- funkSVD(train_matrix, k = 30, gamma = 0.015, lambda = 0.001,min_improvement = 1e-06, min_epochs = 50, max_epochs = 200, verbose = FALSE)
-fsvd_models <- list(fsvd_model_5,fsvd_model_8,fsvd_model_10,fsvd_model_20,fsvd_model_30)
+fsvd_model_40 <- funkSVD(train_matrix, k = 40, gamma = 0.015, lambda = 0.001,min_improvement = 1e-06, min_epochs = 50, max_epochs = 200, verbose = FALSE)
+fsvd_models <- list(fsvd_model_5,fsvd_model_10,fsvd_model_20,fsvd_model_30,fsvd_model_40)
 
 fsvd_model_5_result <- predict_svd(fsvd_model_5, train_matrix, test_matrix)
-fsvd_model_8_result <- predict_svd(fsvd_model_8, train_matrix, test_matrix)
 fsvd_model_10_result <- predict_svd(fsvd_model_10, train_matrix, test_matrix)
 fsvd_model_20_result <- predict_svd(fsvd_model_20, train_matrix, test_matrix)
 fsvd_model_30_result <- predict_svd(fsvd_model_30, train_matrix, test_matrix)
-fsvd_models_results <- list(fsvd_model_5_result,fsvd_model_8_result,fsvd_model_10_result,fsvd_model_20_result)
-
+fsvd_model_40_result <- predict_svd(fsvd_model_40, train_matrix, test_matrix)
+fsvd_models_results <- list(fsvd_model_5_result,fsvd_model_10_result,fsvd_model_20_result,fsvd_model_30_result,fsvd_model_40_result)
+## @knitr ignore-2
 #saveRDS(fsvd_models, "SVD/FSVD_info/fsvd_models.rds")
 #saveRDS(fsvd_models_results, "SVD/FSVD_info/fsvd_models_results.rds")
 
 ## @knitr fsvd_results
-rmse_all <- c('k=5'= fsvd_model_5_result[[3]],'k=8'= fsvd_model_8_result[[3]],'k=10'= fsvd_model_10_result[[3]],'k=20'= fsvd_model_20_result[[3]],'k=30'= fsvd_model_30_result[[3]])
+rmse_all <- c('k=5'= fsvd_model_5_result[[3]],'k=10'= fsvd_model_10_result[[3]],'k=20'= fsvd_model_20_result[[3]],'k=30'= fsvd_model_30_result[[3]],'k=40'= fsvd_model_40_result[[3]])
 rmse_all
 
+## @knitr ignore-3
 ### Get full rating matrix
-fsvd_full_rating_matrix <- rbind(fsvd_model_8_result[[2]],fsvd_model_8_result[[5]])
-fsvd_full_score_matrix <- rbind(fsvd_model_8_result[[1]],fsvd_model_8_result[[4]])
+fsvd_full_rating_matrix <- rbind(fsvd_model_10_result[[2]],fsvd_model_10_result[[5]])
+fsvd_full_score_matrix <- rbind(fsvd_model_10_result[[1]],fsvd_model_10_result[[4]])
 #dimnames(fsvd_full_score_matrix) <- dimnames(data$rating_matrix)
 #dimnames(fsvd_full_rating_matrix) <- dimnames(data$rating_matrix)
-
 ### Test rating predictions
-FunkSVD_results_8 = rep(0,nrow(testing_data))
-FunkSVD_scores_8 = rep(0,nrow(testing_data))
+FunkSVD_results_10 = rep(0,nrow(testing_data))
+FunkSVD_scores_10 = rep(0,nrow(testing_data))
 for ( line in 1:nrow(testing_data)){
   uid = as.character(testing_data[line,]$msno)
   iid = as.character(testing_data[line,]$song_id)
-  FunkSVD_results_8[line] = fsvd_full_rating_matrix[uid,iid]
-  FunkSVD_scores_8[line] = fsvd_full_score_matrix[uid,iid]
+  FunkSVD_results_10[line] = fsvd_full_rating_matrix[uid,iid]
+  FunkSVD_scores_10[line] = fsvd_full_score_matrix[uid,iid]
 }
-testing_data$FunkSVD_results <- FunkSVD_results_8 
-testing_data$FunkSVD_scores <- FunkSVD_scores_8 
-fsvd_test_results <- testing_data[,c(1:3,6,7)]
-
-saveRDS(fsvd_test_results, "SVD/FSVD_info/fsvd_test_results.rds")
+testing_data$FunkSVD_results_10 <- FunkSVD_results_10 
+testing_data$FunkSVD_scores_10 <- FunkSVD_scores_10 
+fsvd_test_results <- testing_data[,c('song_id', 'msno','target','FunkSVD_results_10','FunkSVD_scores_10')]
+#saveRDS(fsvd_test_results, "SVD/FSVD_info/fsvd_test_results.rds")
+#readRDS("SVD/FSVD_info/fsvd_test_results.rds")
 
 ## @knitr fsvd_sample_output
-head(fsvd_test_results,10)
-
-
+head(fsvd_test_results[,1:5],10)
 
 ## @knitr fsvd_prediction_error
-err=0
+err_10 =0
 for (k in 1:nrow(fsvd_test_results)){
-  err = err + abs(fsvd_test_results$FunkSVD_results_8[k] - fsvd_test_results$target[k])
-}
-#Error Rate
-err/nrow(fsvd_test_results)
+  err_10 = err_10 + abs(fsvd_test_results$ FunkSVD_results_10[k] - fsvd_test_results$target[k])}
+#Accuracy
+1-(err_10/nrow(fsvd_test_results))
 
+## @knitr svd_evaluate_result
+evaluation_result <- evaluate(fsvd_test_results$FunkSVD_scores_10,testing_data$target)
+evaluation_result
 
+## @knitr ignore-4
+#saveRDS(evaluation_result,"SVD/FSVD_info/evaluation_result.rds")
 
